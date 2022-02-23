cdab69ca6f51aa2471f4064326bb83ff0d307bcc
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-02-22 21:50:40 -0600

    learned about the back slash in strings

--- dojo-rust/minigrep/src/main.rs
+++ dojo-rust/minigrep/src/main.rs
@@ -7,13 +7,12 @@ fn main() {
     let args = env::args().collect::<Vec<String>>();
     
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     
-    if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
-
+    if let Err(err) = minigrep::run(config) {
+        eprintln!("Application error: {}", err);
         process::exit(1);
     }
 }
@@ -25,3 +24,6 @@ fn main() {
 // Q: Why would I ever want to use Box?
 
 // Q: why can't I use crate instead of minigrep for importing the run fn? 
+
+// Q: What does `\` mean in a string? 
+// A: Rust will not insert a newline at the beginning of the string.
