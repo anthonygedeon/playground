23ce982b68f8eaf46185f9303c2efef9dd10d4e3
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-02-26 23:55:34 -0600

    solution for 1-2

---
+++ books/tcpl/chapter_1/1-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main() {
+        // printf("Doesn\'t exist\c"); // doesn't compile
+        printf("Horizontal Tab\t"); 
+        printf("Backspace\b"); 
+        printf("Carriage Return\r"); 
+        printf("Newline\n"); 
+        printf("Vertical Tab\v"); 
+}
