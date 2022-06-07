04784833e27b5cd7bc63698c3c09a4b9b540bdb8
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-06-07 17:38:29 -0500

    blocking file write

--- top/nodejs/index.js
+++ top/nodejs/index.js
@@ -3,9 +3,8 @@ const path = require("path");
 
 const content = "Some content!";
 
-fs.writeFile(path.join(__dirname, "test.txt"), content, err => {
-        if (err) {
-                console.error(err);
-        }
-
-})
+try {
+        fs.writeFileSync(path.join(__dirname, "test.txt"), content)
+} catch (err) {
+        console.log(err);
+}
