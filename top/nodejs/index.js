923d6f786dc73919992a61f1fbf24b04e4d065e1
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-06-07 00:30:47 -0500

    promise-based file operation

--- top/nodejs/index.js
+++ top/nodejs/index.js
@@ -1,35 +1,21 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("fs/promises");
 
-const fileName = `${__dirname}/test.txt`;
+async function example() {
+    const fileName = `${__dirname}/test.txt`;
 
-// callback hell
-fs.readFile(fileName, "utf8", (err, data) => {
-    if (err) {
+    try {
+        const data = await fs.readFile(fileName, "utf8");
+        console.log(data);
+
+        const content = "My booboo hit the floor";
+        await fs.writeFile(fileName, content);
+        console.log("Wrote some content");
+
+        const newData = await fs.readFile(fileName, "utf8");
+        console.log(newData);
+    } catch (err) {
         console.log(err);
-        return;
     }
+}
 
-    console.log(data);
-
-    const content = "some content!";
-
-    fs.writeFile(fileName, content, err2 => {
-        if (err2) {
-            console.log(err2);
-            return;
-        }
-
-        console.log("write some content!");
-
-        fs.readFile(fileName, "utf8", (err3, data3) => {
-            if (err3) {
-                console.log(err3);
-                return;
-            }
-
-            console.log(data3);
-        })
-    })
-});
-
+example();
