296982c21445ec4e303f38555b728156e97ad3fd
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-06-07 18:03:59 -0500

    play with event emitter

--- top/nodejs/index.js
+++ top/nodejs/index.js
@@ -1,10 +1,21 @@
-const fs = require("fs");
-const path = require("path");
+const EventEmiiter = require("events");
 
-const content = "Some content!";
+const eventEmitter = new EventEmiiter();
 
-try {
-        fs.writeFileSync(path.join(__dirname, "test.txt"), content)
-} catch (err) {
-        console.log(err);
-}
+eventEmitter.on("start", (start, end) => {
+    for (let i = start; i < end; i++) {
+        console.log(`number ${i}`);
+    }
+});
+
+eventEmitter.emit("start", 0, 5);
+
+eventEmitter.once("once", () => console.log("only once"));
+
+eventEmitter.emit("once");
+
+
+eventEmitter.off("start", () => console.log("end connection!"));
+
+console.log(eventEmitter);
+
