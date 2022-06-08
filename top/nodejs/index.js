c437452e19eb511a2d7b60a8410427fa3120d5bd
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-06-07 19:25:10 -0500

    dry-up codebase

--- top/nodejs/index.js
+++ top/nodejs/index.js
@@ -6,6 +6,12 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 const host = "127.0.0.1";
 
+async function handleRequest(res, document) {
+    let fileData = await fs.readFile(path.join(__dirname, document), {encoding: "utf8"});
+    res.write(fileData);
+    res.end();
+}
+
 const server = http.createServer(async (req, res) => {
 
     const uri = new URL(req.url, `http://${req.headers.host}`);
@@ -13,26 +19,19 @@ const server = http.createServer(async (req, res) => {
     res.writeHead(200, {
         "Content-Type": "text/html", 
     });
+    let fileData;
     switch(uri.pathname) {
         case "/":
-            fileData = await fs.readFile(path.join(__dirname, "index.html"), {encoding: "utf8"});
-            res.write(fileData);
-            res.end()
+            handleRequest(res, "index.html").catch((err) => console.error(err));
             break;
         case "/about":
-            fileData = await fs.readFile(path.join(__dirname, "about.html"), {encoding: "utf8"});
-            res.write(fileData);
-            res.end()
+            handleRequest(res, "about.html").catch((err) => console.error(err));
             break;
         case "/contact-me":
-            fileData = await fs.readFile(path.join(__dirname, "contact-me.html"), {encoding: "utf8"});
-            res.write(fileData);
-            res.end()
+            handleRequest(res, "contact-me.html").catch((err) => console.error(err));
             break;
         default:
-            fileData = await fs.readFile(path.join(__dirname, "404.html"), {encoding: "utf8"});
-            res.write(fileData);
-            res.end()
+            handleRequest(res, "404.html").catch((err) => console.error(err));
             break;
     }
 });
