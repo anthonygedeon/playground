41bc9aedb5d1e07dbc229db8b55c7c1d108c5e9d
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-06-07 19:04:36 -0500

    create informational website

--- top/nodejs/index.js
+++ top/nodejs/index.js
@@ -1,21 +1,69 @@
-const EventEmiiter = require("events");
+const fs = require("fs");
+const path = require("path");
+const url = require("node:url");
+const http = require("http");
 
-const eventEmitter = new EventEmiiter();
+const port = process.env.PORT || 3000;
+const host = "127.0.0.1";
 
-eventEmitter.on("start", (start, end) => {
-    for (let i = start; i < end; i++) {
-        console.log(`number ${i}`);
+
+const server = http.createServer((req, res) => {
+
+    const uri = new URL(req.url, `http://${req.headers.host}`);
+
+    res.writeHead(200, {
+        "Content-Type": "text/html", 
+    });
+
+    switch(uri.pathname) {
+        case "/":
+            fs.readFile(path.join(__dirname, "index.html"), "utf8", (err, data) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                res.write(data);
+                res.end();
+
+            });
+            break;
+        case "/about":
+            fs.readFile(path.join(__dirname, "about.html"), "utf8", (err, data) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                res.write(data);
+                res.end();
+
+            });
+            break;
+        case "/contact-me":
+            fs.readFile(path.join(__dirname, "contact-me.html"), "utf8", (err, data) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                res.write(data);
+                res.end();
+
+            });
+            break;
+        default:
+            fs.readFile(path.join(__dirname, "404.html"), "utf8", (err, data) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                res.write(data);
+                res.end();
+
+            });
+            break;
     }
 });
 
-eventEmitter.emit("start", 0, 5);
-
-eventEmitter.once("once", () => console.log("only once"));
-
-eventEmitter.emit("once");
-
-
-eventEmitter.off("start", () => console.log("end connection!"));
-
-console.log(eventEmitter);
+server.listen(port, host, () => {
+    console.log(`listening on port: ${port} on host: ${host}`);
+})
 
