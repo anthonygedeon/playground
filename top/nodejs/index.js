1243ff7309511ec742f4cf9d2d51f348862ded5b
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-06-07 19:18:01 -0500

    use fs promises instead of callbacks

--- top/nodejs/index.js
+++ top/nodejs/index.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
 const url = require("node:url");
 const http = require("http");
@@ -6,59 +6,33 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 const host = "127.0.0.1";
 
-
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
 
     const uri = new URL(req.url, `http://${req.headers.host}`);
 
     res.writeHead(200, {
         "Content-Type": "text/html", 
     });
-
     switch(uri.pathname) {
         case "/":
-            fs.readFile(path.join(__dirname, "index.html"), "utf8", (err, data) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                res.write(data);
-                res.end();
-
-            });
+            fileData = await fs.readFile(path.join(__dirname, "index.html"), {encoding: "utf8"});
+            res.write(fileData);
+            res.end()
             break;
         case "/about":
-            fs.readFile(path.join(__dirname, "about.html"), "utf8", (err, data) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                res.write(data);
-                res.end();
-
-            });
+            fileData = await fs.readFile(path.join(__dirname, "about.html"), {encoding: "utf8"});
+            res.write(fileData);
+            res.end()
             break;
         case "/contact-me":
-            fs.readFile(path.join(__dirname, "contact-me.html"), "utf8", (err, data) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                res.write(data);
-                res.end();
-
-            });
+            fileData = await fs.readFile(path.join(__dirname, "contact-me.html"), {encoding: "utf8"});
+            res.write(fileData);
+            res.end()
             break;
         default:
-            fs.readFile(path.join(__dirname, "404.html"), "utf8", (err, data) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                res.write(data);
-                res.end();
-
-            });
+            fileData = await fs.readFile(path.join(__dirname, "404.html"), {encoding: "utf8"});
+            res.write(fileData);
+            res.end()
             break;
     }
 });
