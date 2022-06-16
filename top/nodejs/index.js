279aef0796c328015b858a40a5c3307b83f592b4
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-06-15 20:43:45 -0500

    update to express

--- top/nodejs/index.js
+++ top/nodejs/index.js
@@ -1,45 +1,31 @@
-const fs = require("fs/promises");
 const path = require("path");
-const url = require("node:url");
-const http = require("http");
 
-const PORT = process.env.PORT || 3000;
-const HOST = "127.0.0.1";
+const express = require("express");
+const app = express();
 
-/** Handles the request from the web server and serves the correct html document.
- * @param res - the response object from the web server.
- * @param doc - the HTML document that will be served back to the web
- * server.
- */
-async function handleRequest(res, doc) {
-    let data = await fs.readFile(path.join(__dirname, doc), {encoding: "utf8"});
-    res.write(data);
-    res.end();
+const port = process.env.PORT || 3000;
+
+function requestFile(doc) {
+	return path.join(__dirname, doc);
 }
 
-const server = http.createServer(async (req, res) => {
-
-    const uri = new URL(req.url, `http://${req.headers.host}`);
-
-    res.writeHead(200, { "Content-Type": "text/html" });
-
-    switch(uri.pathname) {
-        case "/":
-            handleRequest(res, "index.html").catch((err) => console.error(err));
-            break;
-        case "/about":
-            handleRequest(res, "about.html").catch((err) => console.error(err));
-            break;
-        case "/contact-me":
-            handleRequest(res, "contact-me.html").catch((err) => console.error(err));
-            break;
-        default:
-            handleRequest(res, "404.html").catch((err) => console.error(err));
-            break;
-    }
+app.get("/", (req, res) => {
+	res.status(200).sendFile(requestFile("index.html"));
 });
 
-server.listen(PORT, HOST, () => {
-    console.log(`listening on port: ${PORT} on host: ${HOST}`);
+app.get("/about", (req, res) => {
+	res.status(200).sendFile(requestFile("about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+	res.status(200).sendFile(requestFile("contact-me.html"));
+});
+
+app.use((req, res, next) => {
+	res.status(404).sendFile(requestFile("404.html"));
+});
+
+app.listen(port, () => {
+    console.log(`listening on 127.0.0.1:${port}`);
 })
 
