76f6f13eac4464f485dcb97e4ae2e79dca32d38d
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-06-22 22:18:03 -0500

    lesson 1.2

---
+++ learncpp-course/ex-1.2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+        std::cout << "hello world!" << std::endl; // this is a comment
+        return 2; /// this is weird
+        /* 
+         *
+         *
+         *
+         **/
+}
