d37be591b0ae74c183d4108796d670bd3d40b68d
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-06-27 21:03:37 -0500

    lesson 2.7

---
+++ learncpp-course/ex-2.7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int add(int x, int y)
+{
+     return x + y;
+}
+
+int add(int x, int y) // violation of ODR, we've already defined function add
+{
+     return x + y;
+}
+
+int main()
+{
+    int y;
+    int x; // violation of ODR, we've already defined x
+}
