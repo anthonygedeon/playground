4e3a15f520ac217f19b3a8234a8d8e4a9dfb4a03
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-06-27 21:04:35 -0500

    lesson 2.11

---
+++ learncpp-course/ex-2.11-add.cpp
@@ -0,0 +1,5 @@
+#include "ex-2.11-add.h"
+
+int add(int x, int y) {
+        return x + y;
+}
---
+++ learncpp-course/ex-2.11.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "ex-2.11-add.h"
+
+int main()
+{
+    std::cout << "The sum of 3 and 4 is " << add(3, 4) << '\n';
+    return 0;
+}
