273da8ec8cdba011f645038f77de1b0a81be406c
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-07-14 14:43:47 -0500

    lesson 6.2 headers

---
+++ learncpp-course/chp-6/ls-6.2.circle.h
@@ -0,0 +1,8 @@
+#if !defined(CIRLCLE_H)
+#define CIRLCLE_H
+
+namespace basic_math {
+	constexpr double pi { 3.14 };
+}
+
+#endif
---
+++ learncpp-course/chp-6/ls-6.2.growth.h
@@ -0,0 +1,8 @@
+#if !defined(GROWTH_H)
+#define GROWTH_H 
+
+namespace basic_math {
+	constexpr double e{ 2.7 };
+}
+
+#endif
