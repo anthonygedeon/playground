df741ddf44a9800d629aae3e95f9330ae987ab88
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-10-10 23:58:16 -0500

    complete exercise 2 rustlings

--- rustlings/exercises/traits/traits2.rs
+++ rustlings/exercises/traits/traits2.rs
@@ -18,6 +18,13 @@ trait AppendBar {
 }
 
 //TODO: Add your code here
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut vec = self.clone();
+        vec.push("Bar".to_string());
+        vec
+    }
+}
 
 #[cfg(test)]
 mod tests {
