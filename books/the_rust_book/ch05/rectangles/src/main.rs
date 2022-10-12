112de27c4fc5ffc7f03978ea32f929e1ab9b1303
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-10-12 17:05:09 -0500

    constructors, multiple parameters and fns

--- books/the_rust_book/ch05/rectangles/src/main.rs
+++ books/the_rust_book/ch05/rectangles/src/main.rs
@@ -1,16 +1,35 @@
-#[derive(Debug)]
+//#[derive(Debug)]
 struct Rectangle {
     width: u32, 
     height: u32,
 }
 
-fn main() {
-    let scale = 2;
-    let rect1 = Rectangle {
-        width: dbg!(30 + 10 / 20 * 3 * scale), 
-        height: 50, 
-    };
-   
-    dbg!(&rect1);
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size
+        }
+    }
 
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+    
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        self.width > rect.width && self.height > rect.height
+    }
+}
+
+fn main() {
+    let sq1 = Rectangle::square(30);
+    let sq2 = Rectangle::square(10);
+    let sq3 = Rectangle::square(15);
+
+    println!("Can rect1 hold rect2? {}", sq1.can_hold(&sq2));
+    println!("Can rect1 hold rect3? {}", sq1.can_hold(&sq3));
 }
