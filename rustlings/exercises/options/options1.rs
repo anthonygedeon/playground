4c5ad6db85060e0b25cc5fc544f938e4e54714cf
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-10-13 16:56:57 -0500

    complete options

--- rustlings/exercises/options/options1.rs
+++ rustlings/exercises/options/options1.rs
@@ -10,7 +10,12 @@
 fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // We use the 24-hour system here, so 10PM is a value of 22
     // The Option output should gracefully handle cases where time_of_day > 24.
-    ???
+    if time_of_day <= 12 {
+        return Some(5);
+    } else if time_of_day > 24 {
+        return None;
+    }
+     return Some(0);
 }
 
 #[cfg(test)]
@@ -30,6 +35,6 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get at the value contained in the Option?
         let icecreams = maybe_icecream(12);
-        assert_eq!(icecreams, 5);
+        assert_eq!(icecreams, Some(5));
     }
 }
