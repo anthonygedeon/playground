e25636287fb2b1bab03184b0cdb0ce536689d617
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-10-21 18:45:58 -0500

    solve hashmap exercises except for ex3

--- rustlings/exercises/hashmaps/hashmaps3.rs
+++ rustlings/exercises/hashmaps/hashmaps3.rs
@@ -19,6 +19,7 @@
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
+#[derive(Debug)]
 struct Team {
     name: String,
     goals_scored: u8,
@@ -35,12 +36,21 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
+        
+        let team = scores.entry(team_1_name.clone()).or_insert(
+            Team{name: team_2_name.clone(), goals_scored: team_1_score.clone(), goals_conceded: team_2_score.clone()});
+        team.goals_scored += team_1_score;
+        let team = scores.entry(team_2_name.clone()).or_insert(
+            Team{name: team_1_name.clone(), goals_scored: team_2_score.clone(), goals_conceded: team_1_score.clone()});
+        team.goals_conceded += team_2_score;
+
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
     }
+    println!("{:?}", scores);
     scores
 }
 
