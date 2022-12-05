8aa22a09e85337e29fb5ce2633ab7c1da63a8968
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-12-05 00:36:06 -0600

    complete exercise 8+

--- courses/theodinproject/sql_zoo/8+_numeric_examples.sql
+++ courses/theodinproject/sql_zoo/8+_numeric_examples.sql
@@ -32,5 +32,22 @@ SELECT subject, SUM(A_STRONGLY_AGREE)
  GROUP BY subject;
 
 -- 6
+SELECT subject,SUM(A_STRONGLY_AGREE * response) / SUM(response) AS score
+  FROM nss
+ WHERE question='Q22'
+   AND (subject='(H) Creative Arts and Design' OR subject='(8) Computer Science')
+ GROUP BY subject;
+
 -- 7
+SELECT institution, SUM(score * response) / SUM(response) AS score
+  FROM nss
+ WHERE question='Q22'
+   AND (institution LIKE '%Manchester%')
+GROUP BY institution
+ORDER BY institution;
+
 -- 8
+SELECT institution, SUM(sample), SUM((CASE WHEN subject='(8) Computer Science' THEN sample ELSE 0 END)) AS comp
+  FROM nss
+ WHERE question='Q01' AND (institution LIKE '%Manchester%')
+ GROUP BY institution;
