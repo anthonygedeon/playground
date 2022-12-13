66c818753a311206a1c162ed751dc4d938219ea8
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-12-12 23:21:28 -0600

    currently stuck on question 9

--- courses/theodinproject/sql_zoo/4_select_within_select.sql
+++ courses/theodinproject/sql_zoo/4_select_within_select.sql
@@ -11,7 +11,10 @@ WHERE continent = 'Europe' AND (gdp/population) > (
  FROM world 
  WHERE name = 'United Kingdom');
 
--- 3. TODO
+-- 3.
+SELECT name, continent FROM world
+ WHERE continent IN (SELECT continent FROM world WHERE name = 'Argentina' OR name = 'Australia')
+ ORDER BY name;
 
 -- 4.
 SELECT name, population FROM world
@@ -39,8 +42,15 @@ SELECT continent, name, area FROM world x
         WHERE y.continent=x.continent
           AND area>0);
 
--- 8. TODO
+-- 8. 
+SELECT continent c1, (SELECT name FROM world WHERE continent=c1 ORDER BY continent LIMIT 1) as name
+  FROM world
+ GROUP BY continent;
 
 -- 9. TODO
+SELECT name, continent, population 
+  FROM world
+ WHERE population <= 25000000
+-- (SELECT continent FROM world WHERE population <= 25000000)
 
 -- 10. TODO
