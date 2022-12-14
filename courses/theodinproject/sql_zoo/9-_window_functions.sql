e4048b6f458a4e769d5eb986b1a4f4cb69c1c43e
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-12-13 19:34:56 -0600

    complete exercise

--- courses/theodinproject/sql_zoo/9-_window_functions.sql
+++ courses/theodinproject/sql_zoo/9-_window_functions.sql
@@ -26,13 +26,28 @@ SELECT constituency,party, votes,
    AND yr  = 2017
 ORDER BY pos, constituency;
 
--- 5 TODO
+-- 5
+WITH subQ
+AS (SELECT constituency,
+           party,
+           votes,
+           RANK() OVER (PARTITION BY constituency ORDER BY votes DESC) AS rank
+    FROM ge
+    WHERE constituency
+          BETWEEN 'S14000021' AND 'S14000026'
+          AND yr = 2017
+   )
+SELECT constituency,
+       party
+FROM subQ TAB
+WHERE rank = 1
 
--- 6 TODO
- SELECT party, COUNT(*)
-  FROM ge
- WHERE constituency BETWEEN 'S14000021' AND 'S14000026'  
-   AND (yr=2017 AND constituency LIKE 'S%')
-GROUP BY party
-ORDER BY party;
-
+-- 6
+WITH subQ AS (
+    SELECT constituency, party, votes, RANK() OVER (PARTITION BY  constituency ORDER BY votes DESC) AS rank
+    FROM ge
+    WHERE yr = 2017 AND constituency LIKE 'S%')
+SELECT party, COUNT(1)
+FROM subQ
+TAB WHERE rank = 1
+GROUP BY party;
