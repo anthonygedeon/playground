1d2ca0cca20357619099bb10c34f70b7bda89ec0
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-12-20 22:28:26 -0600

    mv into seperate file

---
+++ courses/theodinproject/mastermind/lib/ui.rb
@@ -0,0 +1,13 @@
+module UI
+  CIRCLE = { :fill => "●" , :empty => "○" }
+
+  COLORS = {
+    1 => Rainbow("  1  ").bg(:khaki),
+    2 => Rainbow("  2  ").bg(:maroon),
+    3 => Rainbow("  3  ").bg(:blue),
+    4 => Rainbow("  4  ").bg(:pink),
+    5 => Rainbow("  5  ").bg(:webpurple),
+    6 => Rainbow("  6  ").bg(:plum)
+  }
+
+end
