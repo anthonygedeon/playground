19158bbe0ee57e3f778b4c27794faa66ca4bd56a
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-12-23 20:15:44 -0600

    feat: code parameter mutates @master_code directly so we clone it instead

--- courses/theodinproject/mastermind/lib/clue.rb
+++ courses/theodinproject/mastermind/lib/clue.rb
@@ -1,22 +1,25 @@
 class Clue 
   def self.make(code, guess)
-    code.each_index do |i|
-      if code[i] == guess[i]
-        code[i]  = :fill 
+    secret = code.clone
+    secret.each_index do |i|
+      if secret[i] == guess[i]
+        secret[i]  = :fill 
         guess[i] = :fill 
       end
     end
     
     guess.each_with_index do |g_val, i| 
-      code.each_with_index do |c_val, j|
+      secret.each_with_index do |c_val, j|
         if g_val == c_val && j != i && g_val != :fill
-          code[j] = :empty
+          secret[j] = :empty
           break
         end
       end
     end
 
-    code.reject{ |item| item.is_a? Numeric }
+    pp "secret: #{secret}"
+
+    secret.reject{ |item| item.is_a? Numeric }
   end
 
   private 
