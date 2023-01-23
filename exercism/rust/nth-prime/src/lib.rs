e4dbd26cc34598b8d13a973ab46a08d80ca323c1
Anthony Gedeon <anthony.gedeonn@gmail.com>
2023-01-23 16:51:37 -0600

    complete nth prime exercise

---
+++ exercism/rust/nth-prime/src/lib.rs
@@ -0,0 +1,16 @@
+
+pub fn nth(n: u32) -> u32 {
+    let mut primes = vec![];
+    
+    (2..)
+    .filter(|item: &u32| {
+        if !primes.iter().any(|p| item % p == 0) {
+            primes.push(*item); // memoization
+            true
+        } else {
+            false
+        }
+    })
+    .nth(n as usize)
+    .unwrap()
+}
