e869d3ce10c8773759a77a69b11678670345de0c
Anthony Gedeon <anthony.gedeonn@gmail.com>
2023-03-25 23:54:39 -0500

    add chunks

---
+++ exercism/rust/atbash-cipher/src/lib.rs
@@ -0,0 +1,31 @@
+use std::str;
+
+/// "Encipher" with the Atbash cipher.
+pub fn encode(plain: &str) -> String {
+    let encoded = plain
+        .to_ascii_lowercase()
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .map(|c| {
+            if c.is_ascii_alphabetic() {
+                let wraped_char = (26 - (c as u8 % 97)) + 96;
+                char::from(wraped_char)
+            } else {
+                c
+            }
+        })
+        .collect::<String>();
+
+    let encoded = encoded
+        .as_bytes()
+        .chunks(5)
+        .map(str::from_utf8)
+        .collect::<Result<Vec<&str>, _>>()
+        .unwrap();
+    encoded.join(" ")
+}
+
+/// "Decipher" with the Atbash cipher.
+pub fn decode(cipher: &str) -> String {
+    unimplemented!("Decoding of {cipher:?} in Atbash cipher.");
+}
