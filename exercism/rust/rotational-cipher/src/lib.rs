70cc51ff755cae32be31e585817e34eb870c3ce0
Anthony Gedeon <anthony.gedeonn@gmail.com>
2023-03-30 23:34:01 -0500

    solve rotational-cipher

--- exercism/rust/rotational-cipher/src/lib.rs
+++ exercism/rust/rotational-cipher/src/lib.rs
@@ -2,9 +2,21 @@ pub fn rotate(input: &str, key: i8) -> String {
     input
         .chars()
         .map(|c| match c {
-            'A'..='Z' => todo!(),
-            'a'..='z' => todo!(),
-            _ => (),
+            'A'..='Z' => {
+                let alphabet_num = (((c as u8 % b'A') as u8 + key.unsigned_abs()) % 26) as u8;
+                (b'A' + alphabet_num) as char
+            }
+            'a'..='z' => {
+                let alphabet_num = (c as u8 % b'a').overflowing_add_signed(key);
+
+                if alphabet_num.1 {
+                    let ch = (c as u8 % b'a') + 26 + key.unsigned_abs();
+                    (97 as u8 + (ch % 26) as u8) as char
+                } else {
+                    (b'a' + (alphabet_num.0 % 26)) as char
+                }
+            }
+            _ => c,
         })
         .collect()
 }
