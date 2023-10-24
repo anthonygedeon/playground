3c0d18c3bae572c2384426f1f128eadf89a74c2c
anthonygedeon <anthony.gedeonn@gmail.com>
2023-10-23 21:23:09 -0500

    solve project 5

---
+++ books/c_programming_a_modern_approach/ch02/projects/project_5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int x = 0;
+	scanf("%i", &x);
+	int polynomial = 3 * (x * x * x * x * x) +
+		2 * (x * x * x * x) -
+		5 * (x * x * x) -
+		(x * x) + 7 * x - 6; 
+
+	printf("%i\n", polynomial);
+	return 0;
+}
