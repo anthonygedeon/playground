652b976ad95535f829daef34e26ce3dbab82dc2a
anthonygedeon <anthony.gedeonn@gmail.com>
2023-12-04 18:00:19 -0600

    solve acronym exercise

---
+++ exercism/c/acronym/acronym.c
@@ -0,0 +1,37 @@
+#include "acronym.h"
+#include <ctype.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+enum character_t {
+	ALPHABET = 0, NON_ALPHABET = 1
+};
+
+char *abbreviate(const char *phrase) {
+	if (phrase == NULL || strcmp(phrase, "") == 0) {
+		return NULL;
+	}
+
+	char *acronynm = malloc(1024*sizeof(char));
+	
+	bool state = ALPHABET;
+	size_t acronym_length = 0;
+	for (int i = 0; phrase[i] != '\0'; i++) {
+		if (isalpha(phrase[i]) && state == ALPHABET) {
+			acronynm[acronym_length++] = toupper(phrase[i]);
+			state = NON_ALPHABET;
+		}
+
+		if (phrase[i] == '_' ||
+			phrase[i] == '-' ||
+			phrase[i] == ' ') {
+			state = ALPHABET;
+		}
+	}
+
+	acronynm[acronym_length] = '\0';
+
+	return acronynm;
+}
